999fb532622b8197a56785934c8c248279eb6c59
dennis Kiprono <denniskiprono954@gmail.com>
2020-03-30 04:38:59 -0400

    angular practise

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Goals';
+  goal = 'Watch Finding Nemo'
+  goals:string[];
+
+  constructor(){
+    this.goals = ['Watch finding Nemo', 'Buy cookies', 'Get new phone case']
+  }
+
+}
